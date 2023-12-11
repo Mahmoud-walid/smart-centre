47b07f92385e6bd4d4522b04365c4e7404cf0089
mahmmoud walid mahmoud <modywmbadr@gmail.com>
2023-12-12 01:16:38 +0200

    aboutUsSection

--- js/starScript.js
+++ js/starScript.js
@@ -38,6 +38,39 @@ const numberSlidesAndCards = (numberSlides, numberCards) => {
 };
 numberSlidesAndCards(document.querySelectorAll(".slide").length, 3);
 
-
 // price
 
+// aboutSayCount
+(aboutSayCount = () => {
+  const countPerson = document.querySelector(".countperson");
+  const countPersonDataSet = countPerson.dataset.countperson;
+  countPerson.textContent = `+${countPersonDataSet}`;
+})();
+
+// starsComment
+const setStarComments = (cardNumber) => {
+  const starName = `starscomment${cardNumber}`;
+  const starsElement = document.querySelector(`.${starName}`);
+  const ratingAttribute = starsElement.dataset[starName];
+  // console.log(ratingAttribute);
+  // console.log(starsElement);
+
+  for (let i = 1; i <= 5; i++) {
+    const starElement = starsElement.querySelector(`.star${i}`);
+    if (i <= ratingAttribute) {
+      starElement.style.color = "orange";
+      starElement.classList.remove("fa-regular");
+      starElement.classList.add("fa-solid");
+    } else {
+      starElement.style.color = "grey";
+    }
+  }
+};
+
+const numberCommentCards = (numberCards) => {
+  for (let c = 1; c <= numberCards; c++) {
+    console.log(c);
+    setStarComments(c);
+  }
+};
+numberCommentCards(document.querySelectorAll(".comment").length);
