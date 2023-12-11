069abf7c216b05e92c750a7eacce0d3f383e6f32
mahmmoud walid mahmoud <modywmbadr@gmail.com>
2023-12-11 15:30:28 +0200

    nav responsive

---
+++ js/navBar.js
@@ -0,0 +1,22 @@
+const clickToggle = (() => {
+  let clickedBtn = false;
+  const mobileLinksNav = document.querySelector(".mobileLinksNav");
+
+  return (clicked = (btn) => {
+    if (!clickedBtn) {
+      clickedBtn = true;
+      setTimeout(() => {
+        console.log("block");
+        mobileLinksNav.style.display = "block";
+        mobileLinksNav.style.animationName = "navAnimateUpToDown";
+      }, 0);
+    } else {
+      clickedBtn = false;
+      mobileLinksNav.style.animationName = "navAnimateDownToUp";
+      setTimeout(() => {
+        console.log("none");
+        mobileLinksNav.style.display = "none";
+      }, 1000);
+    }
+  });
+})();
