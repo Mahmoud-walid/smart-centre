f7fe090be3356ed62ff81f57790d5518e625f2d7
mahmmoud walid mahmoud <modywmbadr@gmail.com>
2023-12-14 02:19:02 +0200

    responsive ourTeaching

--- js/carouselScript.js
+++ js/carouselScript.js
@@ -32,6 +32,13 @@ $(".slider").slick({
         slidesToScroll: 1,
       },
     },
+    {
+      breakpoint: 200,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
     // You can unslick at a given breakpoint now by adding:
     // settings: "unslick"
     // instead of a settings object
